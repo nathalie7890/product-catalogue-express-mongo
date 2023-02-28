402083cacea52e1a2185de01630fffc99757e576
nathalie7980 <londonhippos@gmail.com>
2023-02-28 17:25:56 +0800

    update and delete api

--- api/products.js
+++ api/products.js
@@ -13,6 +13,33 @@ router.get("/", async (req, res) => {
   }
 });
 
+//get by id
+router.get("/:id", async (req, res) => {
+  try {
+    const productFound = await Product.findById(req.params.id);
+    if (!productFound)
+      return res.status(404).json({ msg: "Product not found" });
+    else return res.json(productFound);
+  } catch {
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//delete product by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const productFound = await Product.findById(req.params.id);
+    if (!productFound)
+      return res.status(404).json({ msg: "Product not found" });
+
+    await Product.findByIdAndDelete(req.params.id);
+    return res.json({ msg: "Product deleted successfully." });
+  } catch {
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//add product
 router.post("/", async (req, res) => {
   try {
     const {
@@ -39,11 +66,24 @@ router.post("/", async (req, res) => {
       thumbnail,
     });
 
-  
     await product.save();
     return res
       .status(200)
       .json({ product, msg: "New product added successfully" });
+  } catch {
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//update product
+router.put("/:id", async (req, res) => {
+  try {
+    const productFound = await Product.findById(req.params.id);
+    if (!productFound)
+      return res.status(404).json({ msg: "Product not found" });
+
+    await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    return res.json({ productFound, msg: "Product updated successfully" });
   } catch {
     return res.status(500).json({ message: "Server Error" });
   }
